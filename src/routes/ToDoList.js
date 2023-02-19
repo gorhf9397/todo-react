652756bf639be7fd42caea6356c822953e0276f1
seokgeun <trr456@naver.com>
2023-02-19 22:47:52 +0900

    체크된 Todos 만 버튼으로 제거 구현 MUI 설치 및 임포트 스타일 미완료git이슈생성

--- src/routes/ToDoList.js
+++ src/routes/ToDoList.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Checkbox from "@mui/material/Checkbox";
 
 function ToDoList() {
   const [inputValue, setInputValue] = useState("");
@@ -31,10 +34,16 @@ function ToDoList() {
     // spread 연산자 사용
     const newTodos = [...todos];
     newTodos[index].checked = checked;
-    console.log(newTodos[index]);
+    // console.log(newTodos[index]);
     setTodos(newTodos);
   };
-
+  // check된 할일 제거
+  const handleCheckRemove = () => {
+    const newTodos = [...todos];
+    const falseArr = newTodos.filter((e) => !e.checked);
+    // console.log(falseArr);
+    setTodos(falseArr);
+  };
   // useEffect 를 사용하여 컴포넌트 렌더링
   useEffect(() => {
     const storedTodos = localStorage.getItem("todos");
@@ -44,33 +53,49 @@ function ToDoList() {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
 
-  console.log(todos);
+  // console.log(todos);
   return (
     <div>
       <form onSubmit={handleFormSubmit}>
-        <input
-          type="text"
-          placeholder="할일을 입력해주세요"
+        <TextField
+          id="outlined-basic"
+          label="할 일을 입력해주세요."
+          variant="outlined"
+          size="small"
           value={inputValue}
           onChange={handleInputChange}
         />
-        <button type="submit">할일추가</button>
+        <Button variant="contained" type="submit">
+          할일추가
+        </Button>
       </form>
+      <Button variant="contained" onClick={handleCheckRemove}>
+        완료된 일 제거
+      </Button>
       <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>
-            <div className={todo.checked ? "check" : ""}>{todo.text}</div>
-            <input
-              type="checkbox"
-              checked={todo.checked ? "checked" : ""}
-              onChange={({ target: { checked } }) =>
-                handleCheckBox(index, checked)
-              }
-              // 디스트럭처링
-            />
-            <button onClick={() => handleRemoveTodo(index)}>❌</button>
-          </li>
-        ))}
+        {/* 배열에 null or undifined 가 아닌지 확인 */}
+        {todos &&
+          todos.map((todo, index) => (
+            <li key={index}>
+              <div className={todo.checked ? "check" : ""}>{todo.text}</div>
+              <Checkbox
+                size="small"
+                checked={todo.checked ? "checked" : ""}
+                // 디스트럭처링
+                onChange={({ target: { checked } }) =>
+                  handleCheckBox(index, checked)
+                }
+              />
+              <Button
+                size="small"
+                variant="outlined"
+                color="primary"
+                onClick={() => handleRemoveTodo(index)}
+              >
+                ❌
+              </Button>
+            </li>
+          ))}
       </ul>
     </div>
   );
